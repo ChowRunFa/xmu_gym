4c8aa9d8b1022d9b73e23dd94545e97582c32020
wtqiu <wtqiu@qq.com>
2024-12-25 18:05:12 +0800

    init

---
+++ app2.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+# 设置 HTML 文件所在的目录
+HTML_DIR = "./"
+
+@app.route("/")
+def serve_gym_html():
+    # 返回 gym.html 文件
+    return send_from_directory(HTML_DIR, "./templates/index2.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8891)
